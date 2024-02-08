3ad9a1cc9d6eeb4512e0d59dc6370b71fb01319b
Rishabhvenkatesan <32623795+Rishabhvenkatesan@users.noreply.github.com>
2024-02-08 18:47:55 -0500

    added new gnss library

--- PayloadFlightComputer/lib/GPS/GNSS.h
+++ PayloadFlightComputer/lib/GPS/GNSS.h
@@ -9,7 +9,7 @@
  */
 #pragma once
 #include <Arduino.h>
-#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+#include <SparkFun_u-blox_GNSS_v3.h>
 
 class GNSS
 {
@@ -30,62 +30,14 @@ public:
      *
      * @return the latitude value
      */
-    float getLatitude();
+    double getLatitude();
     /**
      * @brief Get the longitude value
      *
      * @return the longitude value
      */
-    float getLongitude();
-
-    /**
-     * @brief Get the status of the GPS Lock
-     * 
-     * @return true 
-     * @return false 
-     */
-    bool getLockStatus();
-
-    /**
-     * @brief Get the number of satellites in view
-     * 
-     * @return uint8_t 
-     */
-    uint8_t getSatellites();
-
-    /**
-     * @brief Get Altitude AGL
-     * 
-     * @return float 
-     */
-    float getAltAGL();
-
-    /**
-     * @brief Get the Altitude MSL
-     * 
-     * @return float 
-     */
-    float getAltMSL();
-
-    /**
-     * @brief Get the current time as a string
-     * 
-     * @return char* Time
-     */
-    String getTime();
-
-    /**
-     * @brief Checks PVT and Fix Status to see if data is ready
-     * 
-     * @return true 
-     * @return false 
-     */
-    bool dataReady();
-
-    void printHwStatus();
+    double getLongitude();
 
 private:
     SFE_UBLOX_GNSS gnss;
-
-    
 };
