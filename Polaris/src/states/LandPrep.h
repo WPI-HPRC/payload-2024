873c381ee3bda41a60598393eaf52303a91a2bf3
R-TJN <rthompsonjordan@gmail.com>
2024-10-06 15:50:12 -0400

    Removed redundant private state variables
    I just found which ones were used using ctrl click and ctrl f , and deleted any that werent

--- Polaris/src/states/LandPrep.h
+++ Polaris/src/states/LandPrep.h
@@ -7,18 +7,6 @@ class LandPrep : public State {
 	public:
 		LandPrep(FlashChip *flash, StateEstimator *stateEstimator, XbeeProSX *xbee,struct Servos *servos, OpenMV *openMV); 
 	private: 
-		// for z acceleration //I need to clarify why these are here 
-		float transitionBufAcc[10]; 
-		uint8_t transitionBufIndAcc = 0;
-		
-		// for vertical velocity
-		int16_t transitionBufVelVert[10];
-		uint8_t transitionBufIndVelVert = 0;
-
-		// Altitude buffer
-		int16_t transitionBufAlt[10];
-		uint8_t transitionBufIndAlt = 0;
-		int16_t altitudePreviousAvg;
 
 		float verticalVelocityBuffer[10] = {0};
 		int bufferIndex = 0;
